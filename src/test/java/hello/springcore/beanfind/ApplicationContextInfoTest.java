432aa706e8c8023b3a321dce5edac709d34cb38d
SQL0318 <rlagudrb97@naver.com>
2024-03-27 13:47:34 +0900

    gogogogogo

--- src/test/java/hello/springcore/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/springcore/beanfind/ApplicationContextInfoTest.java
@@ -3,6 +3,7 @@ package hello.springcore.beanfind;
 import hello.springcore.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContextInfoTest {
@@ -15,6 +16,19 @@ public class ApplicationContextInfoTest {
             Object bean = ac.getBean(beanDefinitionName);
             System.out.println("name=" + beanDefinitionName +" Object="+bean);
         }
+    }
+    @DisplayName("에플리케이션 빈 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
+      //      Role ROLE_APPLICATION (애플리케이션에 등록된 빈)
+      //      Role ROLE_INFRASTRUCTURE (스프링 내부에서 사용하는 빈)
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name="+ beanDefinitionName + ". Object="+bean);
+            }
+        }
     }
 }
